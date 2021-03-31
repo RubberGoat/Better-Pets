080d78c53a9afcdd4c073064facf5e1cd686abe2
Rubber_Goat <z5283527@ad.unsw.edu.au>
2021-03-31 14:51:21 +1100

    Update Pet.java

--- src/main/java/better_pets/Pet.java
+++ src/main/java/better_pets/Pet.java
@@ -10,15 +10,99 @@ package better_pets;
  * @author Rjian
  */
 public class Pet {
+
+    private int id;
+    private String name;
+    private String species;
+    private String colour;
+    private String owner;
+
     // Attributes of Pet
-    protected int id;
-    protected String name;
-    protected String species;
-    protected String colour;
-    protected String owner;
+    public Pet(int id, String name, String species, String colour, String owner) {
+        this.id = id;
+        this.name = name;
+        this.species = species;
+        this.colour = colour;
+        this.owner = owner;
+    }
+ 
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the species
+     */
+    public String getSpecies() {
+        return species;
+    }
+
+    /**
+     * @param species the species to set
+     */
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    /**
+     * @return the colour
+     */
+    public String getColour() {
+        return colour;
+    }
+
+    /**
+     * @param colour the colour to set
+     */
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    /**
+     * @return the owner
+     */
+    public String getOwner() {
+        return owner;
+    }
+
+    /**
+     * @param owner the owner to set
+     */
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+    
+  
 
     // Create a constructor for a Pet class
     
     // Create getters and setters for a Pet class
-    
+
+ 
+   
 }
