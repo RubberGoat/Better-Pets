bc8bad910f05cfada7f3ede55fd8524cd8363d9b
Rubber_Goat <z5283527@ad.unsw.edu.au>
2021-03-31 14:51:11 +1100

    Update LoginController.java

--- src/main/java/better_pets/LoginController.java
+++ src/main/java/better_pets/LoginController.java
@@ -1,7 +1,11 @@
 package better_pets;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -12,19 +16,44 @@ import javafx.scene.control.TextField;
 public class LoginController {
     
     Database database = new Database();
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Label outputLabel;
 
     @FXML
-    private void handleLoginButtonAction(ActionEvent event) throws IOException, SQLException {
+    private void handleLoginButtonAction(ActionEvent event) throws IOException, SQLException, ClassNotFoundException {
         System.out.println("clicked login");
+        
+                
         // Check if username and password exist as a user in the database
         // (Hint: use a function in the Database class)
+        
+       // If the username and password are correct - move to the PetTable screen
 
-        // If the username and password are correct - move to the PetTable screen
-        // Otherwise display text "Incorrect username or password"
+        if (Database.login(usernameField.getText(),passwordField.getText())){
+             
+//            outputLabel.setText("Correct username or password");
+            App.setRoot("PetsScreen");
+             
+  
+        } else {
+              // Otherwise display text "Incorrect username or password"
+                 outputLabel.setText("Incorrect username or password");
+ 
+        }
+               
+                
+    
+       
     }
 
-    private void switchToPetTable() throws IOException {
-        App.setRoot("PetsScreen");
-    }
+//    private void switchToPetTable() throws IOException {
+//        App.setRoot("PetsScreen");
+//    }
     
 }
